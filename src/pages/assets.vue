9ce5c87787635a7116cb72eb8edf071798a4a925
Matvey Melishev <melishevm@gmail.com>
2024-07-06 21:03:06 +0200

    feat: update assets visualization

--- src/pages/assets.vue
+++ src/pages/assets.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { useDBAssetStore } from '@/db/asset/store'
+import { useDBSettingsStore } from '@/db/settings/store'
 
 const dbAssetStore = useDBAssetStore()
-const { assets } = storeToRefs(dbAssetStore)
+const { assets2 } = storeToRefs(dbAssetStore)
+
+const dbSettingsStore = useDBSettingsStore()
+const { settings } = storeToRefs(dbSettingsStore)
 
 function filterAmount(amount: number, currency: string) {
   return new Intl.NumberFormat(undefined, { style: 'currency', currency, currencyDisplay: 'symbol' }).format(amount)
@@ -13,11 +17,19 @@ function filterAmount(amount: number, currency: string) {
   <div>1</div>
 
   <div class="h-screen divide-y border-x bg-neutral-100 p-3 shadow-sm">
-    <div v-for="asset in assets" :key="asset.id" class="flex items-center justify-between p-1">
+    <div v-for="asset in assets2" :key="asset.id" class="flex items-center justify-between p-1">
       <p class="text-xs font-medium text-neutral-950">{{ asset.name }}</p>
-      <p class="text-sm font-semibold tabular-nums text-neutral-950">
-        {{ filterAmount(asset.amount, asset.currency) }}
-      </p>
+      <div class="text-right">
+        <p class="text-sm font-semibold tabular-nums text-neutral-950">
+          {{ filterAmount(asset.amountInPrimaryCurrency, settings.primaryCurrency) }}
+        </p>
+        <p
+          v-if="asset.currency !== settings.primaryCurrency"
+          class="text-xs font-semibold tabular-nums text-neutral-600"
+        >
+          {{ filterAmount(asset.amount, asset.currency) }}
+        </p>
+      </div>
     </div>
   </div>
 
